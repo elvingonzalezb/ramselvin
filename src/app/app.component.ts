996c7eea36b000224124aee9c2d66ecd039c90b3
gelvin <ing.elvingonzalez@gmail.com>
2018-08-20 12:33:24 -0500

    Version 1.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ang-crud';
+  title: string = 'Sistema de Almacen';
+  msg: string = '';
+
+  employees = [
+{'nombre': 'Elvin', position: 'manager', email: 'ing.elvingonzalez@gmail.com'},
+{'nombre': 'Yerina', position: 'Designer', email: 'yerinay@hotmail.com'},
+{'nombre': 'Mimi', position: 'Programmer', email: 'mimimimi@gmail.com'},
+{'nombre': 'Ramses', position: 'Dancing', email: 'elvisramses@gmail.com'}
+  ];
+  model: any = {};
+  model2: any = {};
+  hideUpdate: boolean = true;
+
+  addEmployee(): void {
+    this.employees.push(this.model);
+    this.msg = 'Registro Agregado, satisfactoriamente';
+  }
+
+  deleteEmployee(i): void {
+  var answer = confirm('Seguro de Eliminar');
+  console.log(answer);
+  if(answer){
+   this.employees.splice(i, 1); 
+   this.msg = 'Registro eliminado, Satisfactoriamente';
+  }
+  }
+
+myValue;
+  editEmployee(i): void {
+    this.hideUpdate = false;
+this.model2.nombre = this.employees[i].nombre;
+this.model2.position = this.employees[i].position;
+this.model2.email = this.employees[i].email;
+this.myValue = i;
+  }
+
+
+  updateEmployee(): void {
+    let i = this.myValue;
+    for(let j = 0; j < this.employees.length; j++) {
+      if(i == j) {
+      this.employees[i] = this.model2;
+      this.msg = 'Registro actualizado, Satifactoriamente';
+      this.model2  = {};
+          }
+    }
+}
+
+closeAlert(): void{
+  this.msg = '';
+}
+
 }
