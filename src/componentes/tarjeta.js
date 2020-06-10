219014c864c72b61bed90aea28f978d52338a5bf
elvingonzalezb <ing.elvingonzalez@gmail.com>
2020-06-09 22:08:48 -0500

    Otros componentes

---
+++ src/componentes/tarjeta.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Tarjeta = () => {
+
+    return(
+        <div class="columns is-mobile is-centered">
+           
+        </div> 
+    )
+}
+
+export default Tarjeta;
