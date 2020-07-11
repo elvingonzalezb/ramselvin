38a05c01e8f20c1a83387c892bbbb0a0ebd71216
elvingonzalezb <ing.elvingonzalez@gmail.com>
2020-07-11 00:38:28 -0500

    Segundo

--- src/componentes/imagen.js
+++ src/componentes/imagen.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Imagen = () => {
     return (
         <div class="copyright py-4 text-center text-white">
-            <div class="container"><small>Copyright © 2020</small></div>
+            <div class="container"><small>Copyright ©2020</small></div>
         </div>
     )
 }
