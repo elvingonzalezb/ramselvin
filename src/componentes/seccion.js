c2257d1e89fc718696ee021e8fe333a4a525638a
elvingonzalezb <ing.elvingonzalez@gmail.com>
2020-07-11 00:08:10 -0500

    Segundo

--- src/componentes/seccion.js
+++ src/componentes/seccion.js
@@ -4,13 +4,15 @@ const Seccion = () => {
     return(
         <section class="hero is-primary">
             <div class="hero-body">
-                <div class="container">
-                <h1 class="title">
-                    Backend
-                </h1>
-                <h2 class="subtitle">
-                    Frontend
-                </h2>
+                <div class="container" id="backend">
+                    <h1 class="title">
+                        Backend
+                    </h1>          
+                </div>
+                <div class="container" id="frontend">           
+                    <h2 class="subtitle">
+                        Frontend
+                    </h2>
                 </div>
             </div>
         </section>
